433259e7a80f505f0b7bf24c076fa6a2a193a0e1
ipdmartins <ipdmartins@gmail.com>
2019-12-14 10:32:12 -0300

    ending the course

--- src/networknode.js
+++ src/networknode.js
@@ -221,6 +221,34 @@ app.get('/consensus', function (req, res) {
 
 //Block explorer is an online API that allows to interact with the code inside our Blockchain
 //example (https://blockchain.info/q)
+app.get('/block/:blockHash', function (req, res) {
+    const blockHash = req.params.blockHash;
+    const correctBlock = bitcoin.getBlock(blockHash);
+    res.json({
+        block: correctBlock
+    });
+});
+
+app.get('/transaction/:transactionId', function (req, res) {
+    const transactionId = req.params.transactionId;
+    const transactionData = bitcoin.getTransaction(transactionId);
+    res.json({
+        transaction: transactionData.transaction,
+        block: transactionData.block
+    });
+});
+
+app.get('/address/:address', function (req, res) {
+    const address = req.params.address;
+    const addressData = bitcoin.getAddressData(address);
+    res.json({
+        addressData: addressData
+    });
+});
+
+app.get('/block-explorer', function (req, res) {
+    res.sendFile('./block-explorer/index.html', { root: __dirname });
+});
 
 app.listen(port, function () {
     console.log(`Listening on port ${port}...`);
